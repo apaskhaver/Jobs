e5d819684ce90585018244ab26d5f214a15a2432
apaskhaver <alexandra.paskhaver@gmail.com>
2020-07-09 16:21:01 -0400

    Remove print() statement, add job scrapes to json only if they don't have newline titles

--- jobs/spiders/jobs.py
+++ jobs/spiders/jobs.py
@@ -15,15 +15,15 @@ class JobsSpider(scrapy.Spider):
 
     def start_requests(self):
         for url in self.start_urls:
-            yield Request(url)
+            yield Request(url, dont_filter=False)
 
     def parse(self, response):
         for job in response.xpath('//h2[@class="title"]'):
-           print("job!!!!!!!!!! " + str(job))
-           yield {
-               'title': job.xpath('./a/text()').extract_first(),
-               'link': job.xpath('./a/@href').extract_first()
-           }
+            if (str(job.xpath('./a/text()').extract_first()) != "\n"):
+                yield {
+                    'title': job.xpath('./a/text()').extract_first(),
+                    'link': job.xpath('./a/@href').extract_first()
+                }
 
         next_page_url = response.xpath("//ul[@class='pagination-list']/li[last()]/a/@href").extract_first()
 
